41fbbaaa3eb383e9cd13b50be5a85fe11df1a52f
Vganesh <savijayganesh@gmail.com>
2025-02-10 16:02:13 +0530

    🔧 refac: Moved GetDetails() to utils

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -3,10 +3,10 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/vg006/vtest/internal/types"
+	"github.com/vg006/vtest/internal/utils"
 )
 
 func HomeHandler(e echo.Context) error {
@@ -23,65 +23,11 @@ func DetailsHandler(c echo.Context) error {
 		return c.String(http.StatusBadRequest, "Invalid request")
 	}
 
-	res, err := GetDetails(req)
+	res, err := utils.GetDetails(req)
 	if err != nil {
 		fmt.Println("Error fetching response:", err)
 		return c.String(http.StatusInternalServerError, "Error fetching response")
 	}
 	fmt.Println("Response:", res)
 	return c.JSON(http.StatusOK, res)
-}
-
-func GetDetails(req types.ReqSingleUrl) (types.ResSingleUrl, error) {
-	var res types.ResSingleUrl
-	url := "http://localhost:" + req.Port + req.Endpoint
-
-	fmt.Println("URL:", url)
-	client := http.Client{
-		Timeout: 10 * time.Second,
-	}
-
-	httpReq, err := http.NewRequest(req.Method, url, nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return res, err
-	}
-
-	resp, err := client.Do(httpReq)
-	if err != nil {
-		fmt.Println("Error executing request:", err)
-		res.Headers = append(res.Headers, types.Header{
-			Key:   "error",
-			Value: err.Error(),
-		})
-		return res, err
-	}
-	defer resp.Body.Close()
-	res.StatusCode = resp.StatusCode
-
-	securityHeaders := []string{
-		"Strict-Transport-Security", // HSTS
-		"Content-Security-Policy",   // CSP
-		"X-Frame-Options",           // Prevent clickjacking
-		"X-Content-Type-Options",    // Prevent MIME sniffing
-		"X-XSS-Protection",          // Legacy XSS filter
-		"Referrer-Policy",           // Control referrer info
-		"Permissions-Policy",        // Control browser features
-	}
-
-	for _, headerName := range securityHeaders {
-		if value := resp.Header.Get(headerName); value != "" {
-			res.Headers = append(res.Headers, types.Header{
-				Key:   headerName,
-				Value: value,
-			})
-		} else {
-			res.Headers = append(res.Headers, types.Header{
-				Key:   headerName,
-				Value: "Not set",
-			})
-		}
-	}
-	fmt.Println("Response headers:", res.Headers)
-	return res, nil
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,5 +1,13 @@
 package utils
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/vg006/vtest/internal/types"
+)
+
 func Contains(slice []string, val string) bool {
 	for _, item := range slice {
 		if item == val {
@@ -7,4 +15,58 @@ func Contains(slice []string, val string) bool {
 		}
 	}
 	return false
+}
+
+func GetDetails(req types.ReqSingleUrl) (types.ResSingleUrl, error) {
+	var res types.ResSingleUrl
+	url := "http://localhost:" + req.Port + req.Endpoint
+
+	fmt.Println("URL:", url)
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	httpReq, err := http.NewRequest(req.Method, url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return res, err
+	}
+
+	resp, err := client.Do(httpReq)
+	if err != nil {
+		fmt.Println("Error executing request:", err)
+		res.Headers = append(res.Headers, types.Header{
+			Key:   "error",
+			Value: err.Error(),
+		})
+		return res, err
+	}
+	defer resp.Body.Close()
+	res.StatusCode = resp.StatusCode
+
+	securityHeaders := []string{
+		"Strict-Transport-Security", // HSTS
+		"Content-Security-Policy",   // CSP
+		"X-Frame-Options",           // Prevent clickjacking
+		"X-Content-Type-Options",    // Prevent MIME sniffing
+		"X-XSS-Protection",          // Legacy XSS filter
+		"Referrer-Policy",           // Control referrer info
+		"Permissions-Policy",        // Control browser features
+	}
+
+	for _, headerName := range securityHeaders {
+		if value := resp.Header.Get(headerName); value != "" {
+			res.Headers = append(res.Headers, types.Header{
+				Key:   headerName,
+				Value: value,
+			})
+		} else {
+			res.Headers = append(res.Headers, types.Header{
+				Key:   headerName,
+				Value: "Not set",
+			})
+		}
+	}
+	fmt.Println("Response headers:", res.Headers)
+	return res, nil
 }
