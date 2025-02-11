5b5b79207c4c725ce79a60df0cab4b1288fcbe85
Vganesh <savijayganesh@gmail.com>
2025-02-11 10:08:27 +0530

    ✨ feat: Added ui

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import "../app.css";
+    let { children } = $props();
+</script>
+
+<div class="custom-container flex flex-row flex-nowrap">
+    <div
+        class="w-1/20 bg-gray-400 transition-transform duration-300 ease-in-out transform origin-left hover:scale-x-150"
+    ></div>
+    <div class="w-full">
+        {@render children()}
+    </div>
+</div>
---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div>
+    <h1>Hello</h1>
+    <p>World</p>
+    <p>World</p>
+    <p>World</p>
+    <p>World</p>
+</div>
